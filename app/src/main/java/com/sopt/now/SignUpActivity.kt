74be9465aec395d92b377f677b6caa93661fb44e
arinming <jyandysys1@naver.com>
2024-04-04 16:50:08 +0900

    [Feat] 회원가입 화면 스낵바 조건 함수 구현

--- app/src/main/java/com/sopt/now/SignUpActivity.kt
+++ app/src/main/java/com/sopt/now/SignUpActivity.kt
@@ -1,17 +1,37 @@
 package com.sopt.now
 
+import android.app.Activity
 import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts.*
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 import com.sopt.now.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
+    val startForResult =
+        registerForActivityResult(StartActivityForResult()) { result: ActivityResult ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val intent = result.data
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnSignUp.setOnClickListener {
+            onClickSignUpButton()
+        }
     }
 
+    private fun onClickSignUpButton() {
+        if (binding.edtSignUpId.text.isBlank()) {
+            Snackbar.make(binding.root, "회원 정보를 모두 입력해주세요.", Snackbar.LENGTH_SHORT).show()
+        } else {
+            Snackbar.make(binding.root, "회원가입이 완료되었습니다.", Snackbar.LENGTH_SHORT).show()
+        }
+    }
 }
