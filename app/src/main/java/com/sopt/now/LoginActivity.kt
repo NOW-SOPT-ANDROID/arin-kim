7fd075175b3701e8d412a900c00a780d9d225d6c
arinming <jyandysys1@naver.com>
2024-04-04 15:29:45 +0900

    [Feat] login activity에서 sign up activity 이동

--- app/src/main/java/com/sopt/now/LoginActivity.kt
+++ app/src/main/java/com/sopt/now/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.sopt.now
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.sopt.now.databinding.ActivityLoginBinding
@@ -10,5 +11,16 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        moveToSignUp()
+    }
+
+    private fun moveToSignUp() {
+        val intent = Intent(this, SignUpActivity::class.java)
+
+        binding.btnSignUp.setOnClickListener {
+            startActivity(intent)
+            finish()
+        }
     }
 }
