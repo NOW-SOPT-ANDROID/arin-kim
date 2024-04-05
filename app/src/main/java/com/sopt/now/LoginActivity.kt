69b8e81883ac8dce9c5f964a7581557d91dbce9a
arinming <jyandysys1@naver.com>
2024-04-05 11:24:25 +0900

    [Feat] 회원가입 입력 조건 함수 추가

--- app/src/main/java/com/sopt/now/LoginActivity.kt
+++ app/src/main/java/com/sopt/now/LoginActivity.kt
@@ -1,16 +1,31 @@
 package com.sopt.now
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.sopt.now.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var resultLauncher: ActivityResultLauncher<Intent>
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        resultLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val intent = result.data
+
+                }
+            }
 
         moveToSignUp()
     }
@@ -20,7 +35,6 @@ class LoginActivity : AppCompatActivity() {
 
         binding.btnSignUp.setOnClickListener {
             startActivity(intent)
-            finish()
         }
     }
 }
--- app/src/main/java/com/sopt/now/SignUpActivity.kt
+++ app/src/main/java/com/sopt/now/SignUpActivity.kt
@@ -1,19 +1,28 @@
 package com.sopt.now
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContracts.*
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 import com.sopt.now.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignUpBinding
-    val startForResult =
+    private lateinit var id: String
+    private lateinit var pw: String
+    private lateinit var nickname: String
+    private lateinit var mbti: String
+    private val startForResult =
         registerForActivityResult(StartActivityForResult()) { result: ActivityResult ->
             if (result.resultCode == Activity.RESULT_OK) {
                 val intent = result.data
+                id = intent?.getStringExtra("id") ?: ""
+                pw = intent?.getStringExtra("pw") ?: ""
+                nickname = intent?.getStringExtra("pw") ?: ""
+                mbti = intent?.getStringExtra("mbti") ?: ""
             }
         }
 
@@ -28,10 +37,46 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     private fun onClickSignUpButton() {
-        if (binding.edtSignUpId.text.isBlank()) {
-            Snackbar.make(binding.root, "회원 정보를 모두 입력해주세요.", Snackbar.LENGTH_SHORT).show()
+        if (!isInputValid()) {
+            showSnackBar("회원 정보를 모두 입력해주세요.")
         } else {
-            Snackbar.make(binding.root, "회원가입이 완료되었습니다.", Snackbar.LENGTH_SHORT).show()
+            intent.putExtra("id", id)
+            intent.putExtra("pw", pw)
+            intent.putExtra("nickname", nickname)
+            intent.putExtra("mbti", mbti)
+            startForResult.launch(Intent(this, LoginActivity::class.java))
         }
+    }
+
+    private fun isInputValid(): Boolean {
+        return isIdValid() && isPwValid() && isNicknameValid() && isMbtiValid()
+    }
+
+    private fun isIdValid(): Boolean {
+        val idText = binding.edtSignUpId.text.toString()
+        return idText.isNotBlank() && idText.length in 6..10
+    }
+
+    private fun isPwValid(): Boolean {
+        val pwText = binding.edtSignUpPw.text.toString()
+        return pwText.isNotBlank() && pwText.length in 8..12
+    }
+
+    private fun isNicknameValid(): Boolean {
+        val nicknameText = binding.edtSignUpNickname.text.toString()
+        return nicknameText.isNotBlank() && !nicknameText.contains(" ")
+    }
+
+    private fun isMbtiValid(): Boolean {
+        val mbtiText = binding.edtSignUpMbti.text.toString()
+        return mbtiText.isNotBlank() && mbtiText.length == 4
+    }
+
+    private fun showSnackBar(text: String = "") {
+        Snackbar.make(
+            binding.root,
+            text,
+            Snackbar.LENGTH_SHORT
+        ).show()
     }
 }
