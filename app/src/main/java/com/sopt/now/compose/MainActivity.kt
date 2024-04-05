cb308b6ee476d294f88c58a6be38f57119073c5a
arinming <jyandysys1@naver.com>
2024-04-05 22:32:37 +0900

    Revert "[Feat] 회원가입 레이아웃 구현 (#2)"
    This reverts commit 9bc0b2de8172c306d8c05545e335b7cc365870cd.

--- app/src/main/java/com/sopt/now/compose/MainActivity.kt
+++ app/src/main/java/com/sopt/now/compose/MainActivity.kt
@@ -7,10 +7,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.sopt.now.compose.ui.login.LoginScreen
+import com.sopt.now.compose.ui.NowSoptApp
 import com.sopt.now.compose.ui.theme.NOWSOPTAndroidTheme
 
-class LoginActivity : ComponentActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -19,7 +19,7 @@ class LoginActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    LoginScreen(onNavigateToHome = { /*TODO*/ }) {}
+                    NowSoptApp()
                 }
             }
         }
