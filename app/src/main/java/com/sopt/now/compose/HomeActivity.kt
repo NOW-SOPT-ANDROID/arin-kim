9bc0b2de8172c306d8c05545e335b7cc365870cd
arinming <jyandysys1@naver.com>
2024-04-05 22:32:19 +0900

    [Feat] 회원가입 레이아웃 구현 (#2)

---
+++ app/src/main/java/com/sopt/now/compose/HomeActivity.kt
@@ -0,0 +1,27 @@
+package com.sopt.now.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.sopt.now.compose.ui.home.HomeScreen
+import com.sopt.now.compose.ui.theme.NOWSOPTAndroidTheme
+
+class HomeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NOWSOPTAndroidTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+}
