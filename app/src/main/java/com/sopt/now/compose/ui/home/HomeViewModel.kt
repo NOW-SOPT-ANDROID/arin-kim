bc186b0fdb6a9e7b6ab9e62d4ef9da9a7ffe38f6
arinming <jyandysys1@naver.com>
2024-05-03 16:33:21 +0900

    [Feat] 팔로워 리스트 불러오기 API 구현 (#10)

---
+++ app/src/main/java/com/sopt/now/compose/ui/home/HomeViewModel.kt
@@ -0,0 +1,59 @@
+package com.sopt.now.compose.ui.home
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import com.sopt.now.compose.data.model.Profile
+import com.sopt.now.compose.data.model.ResponseUserDto
+import com.sopt.now.compose.data.model.UserData
+import com.sopt.now.compose.data.module.ServicePool
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class HomeViewModel : ViewModel() {
+    private val followerService by lazy { ServicePool.followerService }
+
+    private val _followerState = MutableStateFlow<List<UserData>>(emptyList())
+    val followerState = _followerState.asStateFlow()
+
+    val friendList = mutableListOf<Profile>()
+
+    init {
+        fetchFollowerList()
+    }
+
+    private fun fetchFollowerList() {
+        followerService.getUserList(page = 0).enqueue(object : Callback<ResponseUserDto> {
+            override fun onResponse(
+                call: Call<ResponseUserDto>,
+                response: Response<ResponseUserDto>,
+            ) {
+                if (response.isSuccessful) {
+                    val data = response.body()?.data
+                    if (data != null) {
+                        _followerState.value = data
+                        mapFollowersToFriendList(data)
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<ResponseUserDto>, t: Throwable) {
+                Log.e("HomeError", "${t.message}")
+            }
+        })
+    }
+
+    fun mapFollowersToFriendList(followers: List<UserData>) {
+        for (follower in followers) {
+            friendList.add(
+                Profile(
+                    profileImage = follower.avatar,
+                    name = "${follower.firstName} ${follower.lastName}",
+                    description = follower.email
+                )
+            )
+        }
+    }
+}
